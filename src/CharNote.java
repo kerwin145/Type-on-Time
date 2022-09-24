65cbd566e09fc1555d914ca631dea1b395624415
Jonathan <jonathan.rong@stonybrook.edu>
2022-09-24 12:45:05 -0400

    CharNote changes & scoreboard start

--- src/CharNote.java
+++ src/CharNote.java
@@ -4,30 +4,50 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 public class CharNote {
-	private int posX, posY, size;
+	private double posX, posY;
+	public static int size;
 	//font size would be the size variable
-	public CharNote() {
-		
+	private String noteType;
+	//represents letter/number of object
+	public CharNote(int x, int y) {
+		posX = x;
+		posY = y;
 	}
 	
 	
 	public void render(Graphics g) {
 		Graphics2D g2d = (Graphics2D)g;	
-		
-		posX = 25;
-		posY = 25;
-		size = 12;
-		
+				
 		Font fntTest = new Font("Verdana", Font.BOLD, size);
 		
 		g2d.draw(getBounds());
-		g.drawString("Hi", posX, posY);
+		g.drawString(noteType, (int)posX, (int)posY);
 
 		
 	}
 	
 	public Rectangle getBounds() {
-		return new Rectangle(posX, posY-size, size, size);
+		return new Rectangle((int)posX, (int)posY-size, size, size);
+	}
+
+
+	public double getPosX() {
+		return posX;
+	}
+
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+
+	public double getPosY() {
+		return posY;
+	}
+
+
+	public void setPosY(int posY) {
+		this.posY = posY;
 	}
 	
 	
