bb4946f2effd852826c7a08a1adeab8b71d96fa9
Liam Gameng <liam.gameng@stonybrook.edu>
2022-09-24 15:02:10 -0400

    editing pos and vel

--- src/CharSpawner.java
+++ src/CharSpawner.java
@@ -16,15 +16,29 @@ public class CharSpawner {
 
 		switch(Game.gameMode) {
 			case HORIZONTAL:
-
+				double random = Math.random();
+				if(random < 0.5) {
+					posX = -charWidth;
+					posY = (int) (Math.round((Math.random()) * Game.HEIGHT) - charHeight / 2);
+					velX = (Game.WIDTH / 2) / time; 
+					velY = ((posX + charHeight / 2) - Game.HEIGHT / 2) / time;
+				} else {
+					posX = Game.WIDTH;
+					posY = (int) (Math.round((Math.random()) * Game.HEIGHT) - charHeight / 2);
+					velX = -(Game.WIDTH / 2) / time; 
+					velY = ((posX + charHeight / 2) - Game.HEIGHT / 2) / time;
+				}
 			case VERTICAL:
-
+				posX = (int) (Math.round((Math.random()) * Game.WIDTH) - charWidth / 2);
+				posY = -charHeight;
+				velX = ((posX + charWidth / 2) - Game.WIDTH / 2) / time;
+				velY = (Game.HEIGHT / 2) / time;
 			case RADIAL:
 				double random = Math.round((Math.random() * (2 * (Game.WIDTH + Game.HEIGHT))));
 				if(random < Game.WIDTH) { // TOP
 					posX = (int) (Math.round((Math.random()) * Game.WIDTH) - charWidth / 2);
 					posY = -charHeight;
-					velX = ((posX + charWidth / 2) - Game.WIDTH / 2) / time
+					velX = ((posX + charWidth / 2) - Game.WIDTH / 2) / time;
 					velY = (Game.HEIGHT / 2) / time;
 				} else if(random < Game.WIDTH + Game.HEIGHT) { // RIGHT
 					posX = Game.WIDTH;
@@ -40,7 +54,7 @@ public class CharSpawner {
 					posX = -charWidth;
 					posY = (int) (Math.round((Math.random()) * Game.HEIGHT) - charHeight / 2);
 					velX = (Game.WIDTH / 2) / time; 
-					velY = ((posX + charHeight / 2) - Game.HEIGHT / 2) / time
+					velY = ((posX + charHeight / 2) - Game.HEIGHT / 2) / time;
 				}
 		}
 	}
