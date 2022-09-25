976db32926c67ccd779f16467187866b7719641f
jsqwertyuiop <jsqwertyuiop04@gmail.com>
2022-09-25 00:22:26 -0400

    Reimplement resetField and escape feature after lost in merge

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -29,9 +29,16 @@ public class InputHandler implements KeyListener, MouseInputListener {
 	public void keyPressed(KeyEvent e) {
 		char noteChar = Character.toUpperCase(e.getKeyChar());
 		if(keysPressed.get(e.getKeyCode()) == false){
-			if(game.levelManager.noteMap.get(noteChar) > 0){
-				game.levelManager.gradeNote(noteChar);
-				keysPressed.put(e.getKeyCode(), true);
+			if(e.getKeyCode() <= 90 && e.getKeyCode() >= 65 && keysPressed.get(e.getKeyCode()) == false){
+				if(game.levelManager.noteMap.get(noteChar) > 0){
+					game.levelManager.gradeNote(noteChar);
+					keysPressed.put(e.getKeyCode(), true);
+				}
+			}
+			if (Game.gameScreen == Game.GameScreen.PLAY){
+				if(e.getKeyCode() == e.VK_ESCAPE)
+					Game.gameScreen = Game.GameScreen.LEVELSELECT;
+					game.levelManager.resetField();
 			}
 		}
 	}
