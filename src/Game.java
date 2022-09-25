d45c7dfd2a169b8e343255a43723ac1f9fe962f6
Liam Gameng <liam.gameng@stonybrook.edu>
2022-09-24 23:41:59 -0400

    formatting

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,12 @@
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-
-
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 
     public static final int WIDTH = 1366;
     public static final int HEIGHT = 768;
@@ -17,10 +14,10 @@ public class Game extends Canvas implements Runnable{
     public final String TITLE = "";
     private boolean running;
     private Thread thread;
-	
+
     public Resources imgs = new Resources();
     public LevelManager levelManager = new LevelManager(this);
-    public CharSpawner  charSpawner = new CharSpawner(this);
+    public CharSpawner charSpawner = new CharSpawner(this);
     public ScoreBoard scoreBoard = new ScoreBoard(this);
     public PopupManager popupManager = new PopupManager(this);
 
@@ -33,30 +30,35 @@ public class Game extends Canvas implements Runnable{
 
     public static int universalTime = 0;
 
-    public static enum GameMode{HORIZONTAL, VERTICAL, RADIAL};
+    public static enum GameMode {
+        HORIZONTAL, VERTICAL, RADIAL
+    };
+
     public static GameMode gameMode = GameMode.HORIZONTAL;
 
-    public static enum GameScreen{TITLE, PLAY, LEVELSELECT, END};
+    public static enum GameScreen {
+        TITLE, PLAY, LEVELSELECT, END
+    };
+
     public static GameScreen gameScreen = GameScreen.END;
-     
-     
+
     public void init() {
         this.requestFocus();
         this.addKeyListener(inputHandler);
         this.addMouseListener(inputHandler);
-   
+
     }
-    	
-	public Game() {
-		running = false;
-	}
-	
-	public void run() {
-	
+
+    public Game() {
+        running = false;
+    }
+
+    public void run() {
+
         this.init();
         long lastTime = System.nanoTime();
         final double amountOfTicks = 60.0;
-        final double ns = Math.pow(10, 9) / amountOfTicks ;
+        final double ns = Math.pow(10, 9) / amountOfTicks;
         double delta = 0.0;
         int updates = 0;
         int frames = 0;
@@ -66,9 +68,9 @@ public class Game extends Canvas implements Runnable{
             delta += (now - lastTime) / ns;
             lastTime = now;
             if (delta >= 1.0) {
-            	
+
                 tick();
-               updates++;
+                updates++;
                 --delta;
                 universalTime++;
             }
@@ -76,7 +78,7 @@ public class Game extends Canvas implements Runnable{
             render();
             if (System.currentTimeMillis() - timer > 1000L) {
                 timer += 1000L;
-          
+
                 System.out.println("Frames: " + frames + ", ticks: " + updates);
                 updates = 0;
                 frames = 0;
@@ -84,69 +86,71 @@ public class Game extends Canvas implements Runnable{
         }
         this.stop();
     }
-    
-	private void tick() {
-        switch(gameScreen){
+
+    private void tick() {
+        switch (gameScreen) {
             case TITLE:
 
-            break;
+                break;
             case PLAY:
                 scoreBoard.tick();
                 levelManager.tick();
                 charSpawner.tick();
                 popupManager.tick();
-            break;
+                break;
+            case END:
+
+            case LEVELSELECT:
+
         }
-	}
-	
-	private void render()  {
-		final BufferStrategy bs = this.getBufferStrategy();
+    }
+
+    private void render() {
+        final BufferStrategy bs = this.getBufferStrategy();
         if (bs == null) {
             this.createBufferStrategy(2);
             return;
         }
         final Graphics g = bs.getDrawGraphics();
-  
+
         g.setColor(Color.white);
         g.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE);
 
-        //rendering begins here
-        switch(gameScreen){
+        // rendering begins here
+        switch (gameScreen) {
             case TITLE:
                 titleScreen.render(g);
-            break;
+                break;
 
             case PLAY:
                 playScreen.render(g);
                 scoreBoard.render(g);
                 levelManager.render(g);
                 popupManager.render(g);
-            break;
+                break;
             case LEVELSELECT:
-            levelSelectScreen.render(g);
-            break;
+                levelSelectScreen.render(g);
+                break;
             case END:
-            endScreen.render(g);
-            break;
+                endScreen.render(g);
+                break;
         }
-        
-       
+
         g.dispose();
         bs.show();
-        
-   
-	}
-    
-	private synchronized void start() {
+
+    }
+
+    private synchronized void start() {
         if (this.running) {
             return;
         }
 
         this.running = true;
-        
+
         (this.thread = new Thread(this)).start();
     }
-    
+
     private synchronized void stop() {
 
         if (!this.running) {
@@ -155,20 +159,18 @@ public class Game extends Canvas implements Runnable{
         this.running = false;
         try {
             this.thread.join();
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    
-    
-	public static void main(String[] args) {
-		System.out.println("It's a great day to debug");
+
+    public static void main(String[] args) {
+        System.out.println("It's a great day to debug");
         final Game game = new Game();
         game.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         game.setMaximumSize(new Dimension(WIDTH, HEIGHT));
         game.setMinimumSize(new Dimension(WIDTH, HEIGHT));
-        
+
         final JFrame frame = new JFrame("Type on Time");
         frame.add(game);
         frame.setDefaultCloseOperation(3);
@@ -176,11 +178,7 @@ public class Game extends Canvas implements Runnable{
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        
+
         game.start();
     }
-
-
-	
-
 }
